256ca18675e51f9ec26b6a100501f088bbdfaf95
sarrahman <sarrahman.dev@gmail.com>
2022-09-21 04:36:43 +0600

    add sample data items :poop:

--- pages/app/data/items/index.tsx
+++ pages/app/data/items/index.tsx
@@ -1,12 +1,71 @@
+import { IconButton } from "../../../../src/components/atoms";
 import { DataTable } from "../../../../src/layout";
 import { DashboardLayout } from "../../../../src/template";
+
+const column = [
+  {
+    title: "Nama Items",
+    dataIndex: "nama_items",
+  },
+  {
+    title: "Brand",
+    dataIndex: "nama_brand",
+  },
+  {
+    title: "Ukuran",
+    dataIndex: "nama_ukuran",
+  },
+  {
+    title: "Stok",
+    dataIndex: "stok",
+  },
+  {
+    title: "aksi",
+    type: "custom",
+    cell: (row: any) => (
+      <div className="text-center">
+        <IconButton
+          other={"mr-3"}
+          backgroundColor="transparent"
+          color="blue"
+          icon="akar-icons:eye"
+          onClick={() => {}}
+        />
+        <IconButton
+          other={"mr-3"}
+          backgroundColor="transparent"
+          color="orange"
+          icon="bxs:edit"
+          onClick={() => {}}
+        />
+        <IconButton
+          backgroundColor="transparent"
+          color="red"
+          icon="bi:trash-fill"
+          onClick={() => {}}
+        />
+      </div>
+    ),
+  },
+];
+
+const data = [
+  {
+    nama_items: "Murano White",
+    nama_brand: "Asia Tiles",
+    nama_ukuran: "40 x 40",
+    stok: 23,
+  },
+];
 
 export default function Items() {
   return (
     <DashboardLayout
       pageName="Items"
       icon="icon-park-outline:ad-product"
-      main={<DataTable column={[]} data={[]} rowsPerPage={[5, 10, 50, 100]} />}
+      main={
+        <DataTable column={column} data={data} rowsPerPage={[5, 10, 50, 100]} />
+      }
     />
   );
 }
--- pages/app/data/ukuran/index.tsx
+++ pages/app/data/ukuran/index.tsx
@@ -1,12 +1,47 @@
+import { IconButton } from "../../../../src/components/atoms";
 import { DataTable } from "../../../../src/layout";
 import { DashboardLayout } from "../../../../src/template";
+
+const column = [
+  {
+    title: "Nama Ukuran",
+    dataIndex: "nama_ukuran",
+  },
+  {
+    title: "aksi",
+    type: "custom",
+    cell: (row: any) => (
+      <div className="text-center">
+        <IconButton
+          other={"mr-3"}
+          backgroundColor="transparent"
+          color="orange"
+          icon="bxs:edit"
+          onClick={() => {}}
+        />
+        <IconButton
+          backgroundColor="transparent"
+          color="red"
+          icon="bi:trash-fill"
+          onClick={() => {}}
+        />
+      </div>
+    ),
+  },
+];
+
+const data = [
+  {
+    nama_ukuran: "40 x 40"
+  }
+]
 
 export default function Ukuran() {
   return (
     <DashboardLayout
       pageName="Ukuran"
       icon="icon-park-outline:ad-product"
-      main={<DataTable column={[]} data={[]} rowsPerPage={[5, 10, 50, 100]} />}
+      main={<DataTable column={column} data={data} rowsPerPage={[5, 10, 50, 100]} />}
     />
   );
 }
