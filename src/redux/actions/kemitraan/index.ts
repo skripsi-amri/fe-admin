0ee37ef5ba1ff6234cd51cb08fadfcc3928941cb
sarrahman <sarrahman.dev@gmail.com>
2022-09-25 04:25:59 +0600

    :fire: menghapus bagian yang tidak diperlukan

--- src/redux/actions/kemitraan/index.ts
+++
@@ -1,6 +0,0 @@
-import { getAllMitra, getMitra, editDataMitra, updateStatusMitra } from "./mitra";
-
-export {
-    getMitra, editDataMitra, updateStatusMitra, getAllMitra
-
-}
--- src/redux/actions/pemasok/index.ts
+++
@@ -1,7 +0,0 @@
-import { getAllKategoriPemasok, editKategoriPemasok, getKategoriPemasok, hapusKategoriPemasok, tambahKategoriPemasok } from "./kategori";
-import { editKlasifikasiPemasok, getAllKlasifikasiPemasok, getKlasifikasiPemasok, hapusKlasifikasiPemasok, tambahKlasifikasiPemasok } from "./klasifikasi";
-import { getAllPemasok, editPemasok, getPemasok, hapusPemasok, tambahPemasok } from "./pemasok";
-
-export {
-    getAllKategoriPemasok, editKategoriPemasok, getKategoriPemasok, hapusKategoriPemasok, tambahKategoriPemasok, getAllPemasok, editPemasok, getPemasok, hapusPemasok, tambahPemasok, editKlasifikasiPemasok, getAllKlasifikasiPemasok, getKlasifikasiPemasok, hapusKlasifikasiPemasok, tambahKlasifikasiPemasok
-}
