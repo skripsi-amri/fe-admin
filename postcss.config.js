f786f53bf223eb06261c125b3fc60425a2901432
sarrahman <sarrahman.dev@gmail.com>
2022-10-01 20:25:32 +0600

    change postcss.config

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,6 @@
-// mungkin akan muncul warning di terminal karena return undefined saat tidak production
-
 module.exports = {
   plugins: [
-    "tailwindcss",
-    process.env.NODE_ENV === "production"
-      ? [
-          "@fullhuman/postcss-purgecss",
-          {
-            content: ["./pages/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        ]
-      : undefined,
-    "postcss-preset-env",
+    'tailwindcss',
+    'postcss-preset-env',
   ],
-};
+}
